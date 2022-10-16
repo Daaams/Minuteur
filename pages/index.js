180360b6a522a0686f598eabcbd9831e56084299
dlevrault <damien.levrault@etu.u-bordeaux.fr>
2022-10-16 21:44:17 +0200

    animation pas tres belle

--- pages/index.js
+++ pages/index.js
@@ -65,6 +65,14 @@ export default function Home() {
           <p className={styles.separateur}>:</p>
           <CubeSecondes secondes={secondes} active={activ}/>
         </div>
+        <svg>
+          <filter id='wavy'>
+            <feTurbulence x="0" y="0" baseFrequency={0.009} numOctaves="5" seed={2}>
+            <animate attributeName='baseFrequency' dur={60} values='0.02;0.005;0.02' repeatCount='indefinite'></animate>
+            </feTurbulence>
+            <feDisplacementMap in="SourceGraphic" scale={30}></feDisplacementMap>
+          </filter>
+        </svg>
       </main>
     </div>
   )
